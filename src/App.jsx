ac042f103849106ad452a8e2e7c202fdfd939e29
Dino Himaj <dinohimaj@rocketmail.com>
2023-08-16 05:32:51 +0200

    fixed tailwind issue, created navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
-import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from './components';
 
 const App = () => {
-  return <div>3D Developer Portfolio</div>;
+  return (
+    <BrowserRouter>
+      <div className='relative z-0 bg-primary'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+          <Navbar />
+        </div>
+
+        <Works />
+        <Feedbacks />
+        <div className='relative z-0'>
+          <Contact />
+          <StarsCanvas />
+        </div>
+      </div>
+    </BrowserRouter>
+  );
 };
 
 export default App;
